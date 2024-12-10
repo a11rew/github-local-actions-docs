e00814e93d53a94b90c9a0c666e580ba1094a30f
Sanjula Ganepola <sanjulagane@gmail.com>
2024-12-10 06:58:41 -0500

    Outline main sections
    Signed-off-by: Sanjula Ganepola <sanjulagane@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,56 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sanjulaganepola.github.io',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'GitHub Local Actions Docs',
+			logo: {
+				src: './public/icon.png',
+				replacesTitle: true
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/SanjulaGanepola/github-local-actions',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/SanjulaGanepola/github-local-actions-docs/edit/main/'
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Start Here',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+						{ label: 'Overview', link: '/' },
+						{ label: 'Installation', link: 'start/installation' }
+					]
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Usage',
+					items: [
+						{ label: 'Components', slug: 'usage/components' },
+						{ label: 'Workflows', slug: 'usage/workflows' },
+						{ label: 'History', slug: 'usage/history' },
+						{ label: 'Settings', slug: 'usage/settings' }
+					]
 				},
+				{
+					label: 'Miscellaneous',
+					items: [
+						{ label: 'Artifacts and Cache', slug: 'miscellaneous/artifactsandcache' },
+						{ label: 'Skipping Jobs and Steps', slug: 'miscellaneous/skippingjobsandsteps' },
+						{ label: 'Custom Container Engine', slug: 'miscellaneous/customcontainerengine' },
+						{ label: 'GitHub Enterprise', slug: 'miscellaneous/githubenterprise' },
+						{ label: 'GitHub Token', slug: 'miscellaneous/githubtoken' },
+						{ label: 'Action Offline Mode', slug: 'miscellaneous/actionofflinemode' }
+					]
+				},
+				{
+					label: 'Help and Support',
+					items: [
+						{ label: 'Troubleshooting', slug: 'help/troubleshooting' },
+						{ label: 'Report an Issue', slug: 'help/reportanissue' },
+						{ label: 'Known Bugs', slug: 'help/knownbugs' }
+					]
+				}
 			],
 		}),
 	],
