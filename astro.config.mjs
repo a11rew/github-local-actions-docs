c3acec62b97471ec0cbbbea832d52e6f086db46f
Sanjula Ganepola <sanjulagane@gmail.com>
2024-12-12 17:58:54 -0500

    Fix images
    Signed-off-by: Sanjula Ganepola <sanjulagane@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sanjulaganepola.github.io',
+	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'GitHub Local Actions Docs',
+			favicon: './icon.png',
 			logo: {
 				src: './public/icon.png',
-				replacesTitle: true
 			},
 			social: {
 				github: 'https://github.com/SanjulaGanepola/github-local-actions',
@@ -29,29 +30,29 @@ export default defineConfig({
 				{
 					label: 'Usage',
 					items: [
-						{ label: 'Components', slug: 'usage/components' },
-						{ label: 'Workflows', slug: 'usage/workflows' },
-						{ label: 'History', slug: 'usage/history' },
-						{ label: 'Settings', slug: 'usage/settings' }
+						{ label: 'Components', link: 'usage/components' },
+						{ label: 'Workflows', link: 'usage/workflows' },
+						{ label: 'History', link: 'usage/history' },
+						{ label: 'Settings', link: 'usage/settings' }
 					]
 				},
 				{
 					label: 'Miscellaneous',
 					items: [
-						{ label: 'Artifacts and Cache', slug: 'miscellaneous/artifactsandcache' },
-						{ label: 'Skipping Jobs and Steps', slug: 'miscellaneous/skippingjobsandsteps' },
-						{ label: 'Custom Container Engine', slug: 'miscellaneous/customcontainerengine' },
-						{ label: 'GitHub Enterprise', slug: 'miscellaneous/githubenterprise' },
-						{ label: 'GitHub Token', slug: 'miscellaneous/githubtoken' },
-						{ label: 'Action Offline Mode', slug: 'miscellaneous/actionofflinemode' }
+						{ label: 'Artifacts and Cache', link: 'miscellaneous/artifactsandcache' },
+						{ label: 'Skipping Jobs and Steps', link: 'miscellaneous/skippingjobsandsteps' },
+						{ label: 'Custom Container Engine', link: 'miscellaneous/customcontainerengine' },
+						{ label: 'GitHub Enterprise', link: 'miscellaneous/githubenterprise' },
+						{ label: 'GitHub Token', link: 'miscellaneous/githubtoken' },
+						{ label: 'Action Offline Mode', link: 'miscellaneous/actionofflinemode' }
 					]
 				},
 				{
 					label: 'Help and Support',
 					items: [
-						{ label: 'Troubleshooting', slug: 'help/troubleshooting' },
-						{ label: 'Report an Issue', slug: 'help/reportanissue' },
-						{ label: 'Known Bugs', slug: 'help/knownbugs' }
+						{ label: 'Troubleshooting', link: 'help/troubleshooting' },
+						{ label: 'Report an Issue', link: 'help/reportanissue' },
+						{ label: 'Known Bugs', link: 'help/knownbugs' }
 					]
 				}
 			],
---
+++ src/content/docs/index.md
@@ -0,0 +1,24 @@
+---
+title: GitHub Local Actions
+description: Run your GitHub Actions locally!
+---
+
+<img src="./icon.png" align="right" width="128" height="128">
+
+<div style="display: flex; gap: 10px;">
+  <a href="https://marketplace.visualstudio.com/items?itemName=SanjulaGanepola.github-local-actions">
+    <img src="https://img.shields.io/visual-studio-marketplace/v/SanjulaGanepola.github-local-actions" alt="Version">
+  </a>
+  <a href="https://marketplace.visualstudio.com/items?itemName=SanjulaGanepola.github-local-actions">
+    <img src="https://img.shields.io/visual-studio-marketplace/i/SanjulaGanepola.github-local-actions" alt="Installs">
+  </a>
+</div>
+
+Run your GitHub Actions locally with the power of the [GitHub Local Actions](https://marketplace.visualstudio.com/items?itemName=SanjulaGanepola.github-local-actions) VS Code extension! Say goodbye to the hassle of committing and pushing changes every time you need to test a workflow. This extension lets you quickly and efficiently run your workflows locally, leveraging the [nektos/act](https://github.com/nektos/act) CLI tool. Enjoy a seamless experience with an interface designed to feel as familiar as the official [GitHub Actions](https://marketplace.visualstudio.com/items?itemName=GitHub.vscode-github-actions) extension.
+
+* 🚀 **Run Workflows/Jobs**: Run entire workflows or specific jobs locally without leaving your editor.
+* ⚡ **Trigger Events**: Trigger standard [GitHub events](https://docs.github.com/en/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows) to run multiple workflows.
+* 📖 **View Workflow Run History**: Track and investigate past workflow logs.
+* ⚙️ **Manage Workflow Settings**: Define secrets, variables, inputs, and runners to be used when executing workflows.
+
+![GitHub Local Actions](https://raw.githubusercontent.com/SanjulaGanepola/github-local-actions/main/images/github-local-actions.gif)
